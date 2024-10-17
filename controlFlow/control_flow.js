efadf83dbfc779ccaec931ffe8dea2ce651c162b
lajinadu <clickndeploy.apps@gmail.com>
2024-10-17 15:06:34 +0200

    Added switch case exercise' '

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -29,3 +29,24 @@ if (isLoggedIn) {
 }
     
 console.log(userMessage)
+
+
+let userType = "admin";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+
+console.log("User Category:", userCategory);
